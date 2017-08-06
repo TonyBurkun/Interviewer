079700c142ae8307c1a56c1d6141d90809acf33d
Tony <burkun.anton@gmail.com>
2017-08-06 21:48:21 +0300

    replace component into separate folders combine all necessary task into one (npm run)

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import Main from './Main';
+import Main from './../Main';
 
 class App extends Component {
     render() {
--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Login from './Login';
-import Home from './Home';
+import Login from './../Login';
+import Home from './../Home';
 
 class Main extends Component {
     render() {
