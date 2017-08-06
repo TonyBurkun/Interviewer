204799a0049ec59a5b3f62b8d96937c950681d0d
vkushnarenko <vkushnarenko@reinvently.com>
2017-08-06 19:15:46 +0300

    Have installed - Rollincode   (Theme)

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV['RAILS_ADMIN_THEME'] = 'rollincode'
+
 module Interviewer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
