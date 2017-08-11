b6715e64f7e9c01000f43cdb55e805130a0a11b3
StarDay2009 <starday2009@mail.ru>
2017-08-11 19:48:49 +0300

    create listener, Login class

--- QAauto/src/main/java/ui/LoginPage.java
+++ QAauto/src/main/java/ui/LoginPage.java
@@ -1,4 +1,22 @@
 package ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
+
+    private final WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+    public LoginPage open() {
+        driver.get("https://qa-interviewer.herokuapp.com/");
+        return this;
+    }
+
+
 }
+
