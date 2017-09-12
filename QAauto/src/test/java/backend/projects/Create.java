57a1e76af92424b482fcaa6aa7f912d78ef37c4b
Oksana <starday2009@mail.ru>
2017-09-12 23:58:24 +0300

    fixes

--- QAauto/src/test/java/backend/projects/Create.java
+++ QAauto/src/test/java/backend/projects/Create.java
@@ -2,6 +2,7 @@ package backend.projects;
 
 
 import com.jayway.restassured.RestAssured;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -9,23 +10,27 @@ import static com.jayway.restassured.RestAssured.given;
 import static org.testng.Assert.assertTrue;
 public class Create {
 
-    @BeforeTest(groups = "backend")
+    @BeforeTest(groups = {"functional", "backend"})
     public void login() {
         RestAssured.baseURI = "https://dev-interviewer.herokuapp.com/";
-
-
+        String IssueKey = "";
     }
 
+
     @Test(groups = {"functional", "backend"})
-    public void subTaskCRUD() throws InterruptedException {
-        return
-                given().
+    public void createPro() throws InterruptedException {
+
+        //IssueKey =
+                        given().
                         header("Content-Type", "application/json").
-                        body().
+                      //  body("1").
                         when().
                         post("api/v1/projects/").
                         then().
-                        extract().
-                        path("session.value");
+                        statusCode(200).
+                        extract().path("key");
+                     //   extract().response();
+       // Assert.assertEquals(,);
+      //  System.out.println(IssueKey);
     }
 }
