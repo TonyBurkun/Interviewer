a59a2a00943777044a2cc617865299fe40978748
TonyBurkun <burkun.anton@gmail.com>
2017-09-18 23:18:44 +0300

    ProjectList. Added project title in Projects List.

--- frontend/src/components/ProjectsList/ProjectsList.js
+++ frontend/src/components/ProjectsList/ProjectsList.js
@@ -42,7 +42,7 @@ class ProjectsList extends Component {
         });
     }
 
-    deleteProject(){
+    deleteProject() {
         this.closeModalConfirm();
         const {dispatch} = this.props;
         dispatch(getProjects(this.state.currentProjectID));
@@ -65,55 +65,39 @@ class ProjectsList extends Component {
 
         if (projects) {
             sortedProjects = projects.sort(compareTitle) || {};
-            projectsToDisplay = sortedProjects.map((value, index) =>
-                <Panels
-                    key={index}
-                    id={value.id}
-                    description={value.description}
-                    showEditBtn={true}
-                    showDeleteBtn={true}
-                    callDelete={(event) => this.openModalConfirm(value.id)}
-                    callEdit={(event) => this.switchToEditMode(value.id)}
-                />
-            );
+            projectsToDisplay = sortedProjects.map((value, index) => {
 
+                    const panelTitle = (
+                        <div className="custom-panel-title project-list-item">
+                            <div className="custom-panel-title__right-side">
+                                <div className="panel-collapse-btn">
+                                    <span className="panel-collapse-btn__title btn-js">Expand</span>
+                                    <span className="fa fa-angle-right panel-collapse-btn__arrow arrow-js"/>
+                                </div>
+                            </div>
+                            <div className="custom-panel-title__left-side">
+                                <div className="vacancy-info-block">
+                                    <div className="vacancy-info-block__item">{value.title}</div>
+                                </div>
+                            </div>
+                        </div>
+                    );
 
+                    return (
+                        <Panels
+                            key={index}
+                            id={value.id}
+                            titleConst={panelTitle}
+                            description={value.description}
+                            showEditBtn={true}
+                            showDeleteBtn={true}
+                            callDelete={(event) => this.openModalConfirm(value.id)}
+                            callEdit={(event) => this.switchToEditMode(value.id)}
+                        />
+                    )
 
-
-            // projectsToDisplay = (sortedProjects) => {
-            //
-            //     let resultArr = projects.map(function (item, index) {
-            //         const panelTitle = (
-            //             <div className="custom-panel-title project-list-item">
-            //                 <div className="custom-panel-title__right-side">
-            //                     <div className="panel-collapse-btn">
-            //                         <span className="panel-collapse-btn__title btn-js">Expand</span>
-            //                         <span className="fa fa-angle-right panel-collapse-btn__arrow arrow-js"/>
-            //                     </div>
-            //                 </div>
-            //                 <div className="custom-panel-title__left-side">
-            //                     <div className="vacancy-info-block">
-            //                         <div className="vacancy-info-block__item">{item.title}</div>
-            //                     </div>
-            //                 </div>
-            //             </div>
-            //         );
-            //
-            //         return (
-            //             <Panels
-            //                 key={index}
-            //                 id={item.id}
-            //                 titleConst={panelTitle}
-            //                 description={item.description}
-            //                 showEditBtn={true}
-            //                 showDeleteBtn={true}
-            //                 callDelete={() => this.openModalConfirm()}
-            //             />
-            //         )
-            //     });
-            //
-            //     return (resultArr)
-            // }
+                }
+            )
 
         } else {
             projectsToDisplay = "";
@@ -168,7 +152,7 @@ class ProjectsList extends Component {
     }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
     return {
         newProject: state.project,
         notifications: state.notifications
